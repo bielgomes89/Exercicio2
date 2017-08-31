d2eacba29f5375a386b3148f6662bbcdf7608feb
gabriel.gomes2 <gabriel.gomes2@PAC-LAB-A-10.unisul.br>
2017-08-31 20:56:17 -0300

    jefinho gambi

--- src/br/unisul/prog2/exercicio2/Exercicio2.java
+++ src/br/unisul/prog2/exercicio2/Exercicio2.java
@@ -7,7 +7,6 @@ import javax.swing.JOptionPane;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author gabriel.gomes2
@@ -18,9 +17,60 @@ public class Exercicio2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int opcao = Integer.parseInt(JOptionPane.showInputDialog("Insira a opção desejada: \n" + "1 - Deletar Jogo" + "\n" 
-        + "2 - Inserir Jogo" + "\n"
-        + "3 - Seleção de Jogos"));
+        String opcao = JOptionPane.showInputDialog("Insira a opção desejada: \n" + "1 - Cadastros" + "\n"
+                + "2 - Pedidos" + "\n"
+                + "3 - Relatórios");
+
+        switch (opcao) {
+            case "1":
+                int cadastro = Integer.parseInt(JOptionPane.showInputDialog("Insira a opção desejada: \n" + "1 - Clientes" + "\n"
+                        + "2 - Produtos"));
+
+                switch (cadastro) {
+                    case 1:
+//                        Clientes
+                        break;
+
+                    case 2:
+//                        Produtos
+                        break;
+                }
+
+            case "2":
+                int pedido = Integer.parseInt(JOptionPane.showInputDialog("Insira a opção desejada: \n" + "1 - Efetuar Pedido" + "\n"
+                        + "2 - Consultar Pedido"));
+
+                switch (pedido) {
+                    case 1:
+//                       Efetuar Pedidos
+                        break;
+
+                    case 2:
+//                       Listar Produtos
+                        break;
+                }
+                
+            case 3:
+                int relatorio = Integer.parseInt(JOptionPane.showInputDialog("Insira a opção desejada: \n" + "1 - Listagem de Clientes" + "\n"
+                        + "2 - Listagem de Produtos \n"
+                        + "3 - Listagem de Pedidos"));
+
+                switch (relatorio) {
+                    case 1:
+//                       Listagem de Clientes
+                        break;
+
+                    case 2:
+//                       Listagem de Produtos
+                        break;
+                    case 3:
+//                        Listagem de Pedidos
+                        break;
+                }
+                
+            default:
+                System.out.println("Digite uma opção valida!");
+        }
+
     }
-    
 }
