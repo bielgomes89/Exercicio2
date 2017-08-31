f6ee9ada570b1aa7e17757ef7c660c2e9d118c0b
gabriel.gomes2 <gabriel.gomes2@PAC-LAB-A-10.unisul.br>
2017-08-31 20:18:44 -0300

    jefinho gambi

---
+++ src/br/unisul/prog2/exercicio2/Pedido.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.unisul.prog2.exercicio2;
+
+/**
+ *
+ * @author gabriel.gomes2
+ */
+public class Pedido {
+    public int codigo;
+    public int codCliente;
+    public String descricao;
+    public double valorTotal; 
+    public String dataPedido;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getCodCliente() {
+        return codCliente;
+    }
+
+    public void setCodCliente(int codCliente) {
+        this.codCliente = codCliente;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public String getDataPedido() {
+        return dataPedido;
+    }
+
+    public void setDataPedido(String dataPedido) {
+        this.dataPedido = dataPedido;
+    }
+    
+    
+}
--- src/br/unisul/prog2/exercicio2/Produto.java
+++ src/br/unisul/prog2/exercicio2/Produto.java
@@ -15,5 +15,47 @@ public class Produto {
     public String marca;
     public String preco;
     public String quantidade;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getPreco() {
+        return preco;
+    }
+
+    public void setPreco(String preco) {
+        this.preco = preco;
+    }
+
+    public String getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(String quantidade) {
+        this.quantidade = quantidade;
+    }
+    
+    
     
 }
